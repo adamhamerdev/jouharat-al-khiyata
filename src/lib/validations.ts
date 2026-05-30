ce4d925e5415d0b18fa0458e89c5f114b2ae4730
adamhamerdev <adamkiller231@gmail.com>
2026-05-30 13:26:16 +0100

    Delete src directory

--- src/lib/validations.ts
+++
@@ -1,7 +0,0 @@
-export function validatePhone(phone: string): boolean {
-  return /^(05|06|07)\d{8}$/.test(phone)
-}
-
-export function validateName(name: string): boolean {
-  return name.trim().length > 0
-}
